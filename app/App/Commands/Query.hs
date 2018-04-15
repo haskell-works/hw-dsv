b1229b9add7ed520332ca4b53fd95e1770de34cd
John Ky <newhoggy@gmail.com>
2018-04-15 22:32:07 +1000

    Command line arguments for runQuery command

--- app/App/Commands/Query.hs
+++ app/App/Commands/Query.hs
@@ -2,10 +2,27 @@ module App.Commands.Query
   ( cmdQuery
   ) where
 
+import Control.Applicative
+import Data.Monoid
 import Options.Applicative
 
-runQuery :: IO ()
-runQuery = return ()
+runQuery :: Bool -> [Int] -> FilePath -> IO ()
+runQuery _createIndex _column _filePath = do
+  -- toInterestBitsVector
+  return ()
+  return ()
 
 cmdQuery :: Mod CommandFields (IO ())
-cmdQuery = command "query"  (info (pure runQuery) idm)
+cmdQuery = command "query"  $ flip info idm $ runQuery
+    <$> switch (long "create-index")
+    <*> many
+        ( option auto
+          (   long "column"
+          <>  short 'k'
+          <>  help "Column to select"
+          <>  metavar "COLUMN INDEX" ))
+    <*> strOption
+          (   long "file"
+          <>  help "Separated Value file"
+          <>  metavar "STRING"
+          )
