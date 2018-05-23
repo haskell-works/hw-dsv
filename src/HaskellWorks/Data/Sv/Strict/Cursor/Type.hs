3c2bee865a4d314686826e1b9bbc6c9f010b8009
John Ky <newhoggy@gmail.com>
2018-05-23 12:31:48 +1000

    Remove unused type LazyCursor

--- src/HaskellWorks/Data/Sv/Strict/Cursor/Type.hs
+++ src/HaskellWorks/Data/Sv/Strict/Cursor/Type.hs
@@ -11,14 +11,11 @@ import Data.Word
 import HaskellWorks.Data.Container
 import HaskellWorks.Data.Positioning
 
-import qualified Data.ByteString.Lazy as LBS
-
 data SvCursor t s = SvCursor
   { svCursorDelimiter :: Elem t
   , svCursorText      :: !t
   , svCursorMarkers   :: !s
   , svCursorPosition  :: !Word64
-  -- TODO See if there is a faster way to compute popCount field
   , svCursorPopCount  :: !Count
   }
 
@@ -35,12 +32,5 @@ data SvCursor2 t s = SvCursor2
 
 deriving instance (Eq   (Elem t), Eq   t, Eq   s) => Eq   (SvCursor2 t s)
 deriving instance (Show (Elem t), Show t, Show s) => Show (SvCursor2 t s)
-
-data LazyCursor = LazyCursor
-  { lazyCursorDelimiter    :: !Word8
-  , lazyCursorText         :: LBS.ByteString
-  , lazyCursorInterestBits :: [Word64]
-  , lazyCursorPosition     :: !Word64
-  } deriving (Eq, Show)
 
 data SvMode = SvUnquoted | SvQuoted deriving (Eq, Show)
