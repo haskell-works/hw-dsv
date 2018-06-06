22f03c44eabcb50518a7cb69f49034dad6145ae4
John Ky <newhoggy@gmail.com>
2018-06-06 18:09:06 +1000

    Remove unused field

--- src/HaskellWorks/Data/Dsv/Lazy/Cursor/Type.hs
+++ src/HaskellWorks/Data/Dsv/Lazy/Cursor/Type.hs
@@ -6,9 +6,8 @@ import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Vector.Storable as DVS
 
 data DsvCursor = DsvCursor
-  { dsvCursorDelimiter :: Word8
-  , dsvCursorText      :: !LBS.ByteString
-  , dsvCursorMarkers   :: ![DVS.Vector Word64]
-  , dsvCursorNewlines  :: ![DVS.Vector Word64]
-  , dsvCursorPosition  :: !Word64
+  { dsvCursorText     :: !LBS.ByteString
+  , dsvCursorMarkers  :: ![DVS.Vector Word64]
+  , dsvCursorNewlines :: ![DVS.Vector Word64]
+  , dsvCursorPosition :: !Word64
   } deriving (Eq, Show)
