65f8a4d427b05739182809a0750452614eb8dd4a
George Wilson <george@wils.online>
2018-06-11 11:21:26 +1000

    Weigh lazy hw-dsv parsing

--- weigh/Space.hs
+++ weigh/Space.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString.Lazy                as LBS
 import qualified Data.Csv                            as CSV
 import qualified Data.Vector                         as DV
 import qualified HaskellWorks.Data.Dsv.Strict.Cursor as SVS
+import qualified HaskellWorks.Data.Dsv.Lazy.Cursor   as SVL
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 
@@ -16,11 +17,18 @@ repeatedly f a = a:case f a of
   Just b  -> repeatedly f b
   Nothing -> []
 
-loadCsv :: FilePath -> IO (DV.Vector (DV.Vector ByteString))
-loadCsv filePath = do
+loadCsvStrict :: FilePath -> IO (DV.Vector (DV.Vector ByteString))
+loadCsvStrict filePath = do
   c <- SVS.mmapCursor ',' False filePath
 
   return $ SVS.toVectorVector c
+
+loadCsvLazy :: FilePath -> IO (DV.Vector (DV.Vector LBS.ByteString))
+loadCsvLazy filePath = do
+  bs <- LBS.readFile filePath
+  let c = SVL.makeCursor ',' bs
+
+  return $ SVL.toVectorVector c
 
 main :: IO ()
 main = do
@@ -33,8 +41,11 @@ main = do
           case r of
             Left _  -> error "Unexpected parse error"
             Right v -> pure v
-      , action "hw-dsv/decode/Vector ByteString" $ do
-          v <- loadCsv infp :: IO (Vector (Vector ByteString))
+      , action "hw-dsv/decode/strict/Vector ByteString" $ do
+          v <- loadCsvStrict infp :: IO (Vector (Vector ByteString))
+          pure v
+      , action "hw-dsv/decode/lazy/Vector ByteString" $ do
+          v <- loadCsvLazy infp :: IO (Vector (Vector LBS.ByteString))
           pure v
       ]
   return ()
