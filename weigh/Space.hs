35d01bd13745d010b2345ff9d69c51f70994c9e3
George Wilson <george@wils.online>
2018-06-11 23:32:39 +1000

    Make weigh case labels distinct

--- weigh/Space.hs
+++ weigh/Space.hs
@@ -25,12 +25,12 @@ loadCsvStrict filePath = do
 
   return $ SVS.toVectorVector c
 
-loadCsvLazy :: FilePath -> IO (DV.Vector (DV.Vector LBS.ByteString))
+loadCsvLazy :: FilePath -> IO [DV.Vector LBS.ByteString]
 loadCsvLazy filePath = do
   bs <- LBS.readFile filePath
   let c = SVL.makeCursor ',' bs
 
-  return $ SVL.toVectorVector c
+  return $ SVL.toListVector c
 
 main :: IO ()
 main = do
@@ -38,18 +38,18 @@ main = do
   mainWith $ do
     setColumns [Case, Allocated, Max, Live, GCs]
     sequence_
-      [ action "cassava/decode/strict/Vector ByteString" $ do
+      [ action "cassava strict" $ do
           r <- fmap (CSV.decode CSV.HasHeader) (LBS.readFile infp) :: IO (Either String (Vector (Vector ByteString)))
           case r of
             Left _  -> error "Unexpected parse error"
             Right v -> pure v
-      , action "cassava/decode/streaming/Vector ByteString" $ do
-          fmap (DV.fromList . F.toList . CSS.decode CSS.HasHeader) (LBS.readFile infp) :: IO (Vector (Vector ByteString))
-      , action "hw-dsv/decode/strict/Vector ByteString" $ do
+      , action "cassava streaming" $ do
+          fmap (F.toList . CSS.decode CSS.HasHeader) (LBS.readFile infp) :: IO [Vector ByteString]
+      , action "hw-dsv strict" $ do
           v <- loadCsvStrict infp :: IO (Vector (Vector ByteString))
           pure v
-      , action "hw-dsv/decode/lazy/Vector ByteString" $ do
-          v <- loadCsvLazy infp :: IO (Vector (Vector LBS.ByteString))
+      , action "hw-dsv lazy" $ do
+          v <- loadCsvLazy infp :: IO [Vector LBS.ByteString]
           pure v
       ]
   return ()
