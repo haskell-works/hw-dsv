6e4c7fc4ca914678d7b3939814f95b4828e046c0
John Ky <newhoggy@gmail.com>
2018-08-07 21:38:10 +1000

    Add build via Visual Studio Code support

---
+++ project.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+STACK_FLAGS="
+  --flag bits-extra:bmi2
+  --flag hw-rankselect-base:bmi2
+  --flag hw-rankselect:bmi2
+  --flag hw-simd:bmi2
+  --flag hw-simd:avx2
+  --flag hw-dsv:bmi2
+  --flag hw-dsv:avx2
+"
+
+case $1 in
+  build)
+    stack build \
+      --test --no-run-tests --bench --no-run-benchmarks \
+      $STACK_FLAGS
+    ;;
+
+  test)
+    stack test \
+      $STACK_FLAGS
+    ;;
+
+  bench)
+    stack bench \
+      $STACK_FLAGS
+    ;;
+
+  repl)
+    stack repl \
+      $STACK_FLAGS
+    ;;
+esac
