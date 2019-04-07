16dbc89c7d7ec718e67ec126a350b7e5f48f35e0
John Ky <newhoggy@gmail.com>
2019-04-08 08:43:13 +1000

    Fix hlint annotation

--- test/HaskellWorks/Data/Dsv/BroadwordSpec.hs
+++ test/HaskellWorks/Data/Dsv/BroadwordSpec.hs
@@ -14,7 +14,7 @@ import Test.Hspec
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
-{-# ANN module ("HLint: redundant bracket"          :: String) #-}
+{-# ANN module ("HLint: ignore Redundant bracket"   :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.Dsv.BroadwordSpec" $ do
--- test/HaskellWorks/Data/DsvSpec.hs
+++ test/HaskellWorks/Data/DsvSpec.hs
@@ -15,7 +15,7 @@ import qualified HaskellWorks.Data.Dsv.Strict.Cursor.Internal.Reference as SVS
 
 {-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
-{-# ANN module ("HLint: redundant bracket"          :: String) #-}
+{-# ANN module ("HLint: ignore Redundant bracket"   :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.DsvSpec" $ do
