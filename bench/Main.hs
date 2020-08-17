71706bb2a7e9b484577be712ddfaaa0ac7726f2e
John Ky <john.ky@iohk.io>
2020-08-17 17:53:45 +1000

    Upgrade to optparse-applicative-0.16

--- bench/Main.hs
+++ bench/Main.hs
@@ -24,6 +24,8 @@ import qualified HaskellWorks.Data.Dsv.Strict.Cursor.Internal.Reference as SVS
 import qualified HaskellWorks.Data.FromForeignRegion                    as IO
 import qualified HaskellWorks.Data.RankSelect.CsPoppy                   as RS
 
+{- HLINT ignore "Reduce duplication"  -}
+
 loadCassavaStrict :: FilePath -> IO (Vector (Vector ByteString))
 loadCassavaStrict filePath = do
   !bs <- LBS.readFile filePath
@@ -39,8 +41,7 @@ loadCassavaStreaming filePath = do
   pure r
 
 loadHwsvStrictIndex :: FilePath -> IO (SVS.DsvCursor ByteString RS.CsPoppy)
-loadHwsvStrictIndex filePath =
-  SVS.mmapCursor comma True filePath
+loadHwsvStrictIndex = SVS.mmapCursor comma True
 
 loadHwsvStrict :: FilePath -> IO (Vector (Vector ByteString))
 loadHwsvStrict filePath = SVS.toVectorVector <$> SVS.mmapCursor comma True filePath
@@ -63,13 +64,12 @@ loadHwsvLazy filePath = do
 makeBenchCsv :: IO [Benchmark]
 makeBenchCsv = do
   entries <- listDirectory "data/bench"
-  let files = ("data/bench/" ++) <$> (".csv" `isSuffixOf`) `filter` entries
+  let files = ("data/bench/" ++) <$> (".csv" `L.isSuffixOf`) `filter` entries
   benchmarks <- forM files $ \file -> return $ mempty
     <> [bench ("cassava/decode/via-strict/"       <> file) (nfIO (loadCassavaStrict    file))]
     <> [bench ("cassava/decode/via-streaming/"    <> file) (nfIO (loadCassavaStreaming file))]
     <> [bench ("hw-dsv/decode/via-strict/"        <> file) (nfIO (loadHwsvStrict       file))]
     <> [bench ("hw-dsv/decode/via-lazy/"          <> file) (nfIO (loadHwsvLazy         file))]
-
     <> [bench ("hw-dsv/decode/via-strict/index/"  <> file) (nfIO (loadHwsvStrictIndex  file))]
     <> [bench ("hw-dsv/decode/via-lazy/index/"    <> file) (nfIO (loadHwsvLazyIndex    file))]
   return (join benchmarks)
@@ -77,7 +77,7 @@ makeBenchCsv = do
 makeBenchW64s :: IO [Benchmark]
 makeBenchW64s = do
   entries <- listDirectory "data/bench"
-  let files = ("data/bench/" ++) <$> (".csv" `isSuffixOf`) `filter` entries
+  let files = ("data/bench/" ++) <$> (".csv" `L.isSuffixOf`) `filter` entries
   benchmarks <- forM files $ \file -> return
     [ env (IO.mmapFromForeignRegion file) $ \v -> bgroup "Creating bit index from mmaped file" $ mempty
       <> [bench ("mkIbVector                  with sum" <> file) (whnf (DVS.foldl (+) 0 . SVS.mkIbVector comma) v)]
@@ -87,7 +87,7 @@ makeBenchW64s = do
 makeBenchMkInterestBits :: IO [Benchmark]
 makeBenchMkInterestBits = do
   entries <- listDirectory "data/bench"
-  let files = ("data/bench/" ++) <$> (".csv" `isSuffixOf`) `filter` entries
+  let files = ("data/bench/" ++) <$> (".csv" `L.isSuffixOf`) `filter` entries
   benchmarks <- forM files $ \file -> return
     [ env (IO.mmapFromForeignRegion file) $ \(v :: DVS.Vector Word64) -> bgroup "Loading lazy byte string into Word64s" $ mempty
       <> [bench ("mkIbVector                  with sum" <> file) (whnf (DVS.foldr (+) 0 . SVS.mkIbVector        pipe) v)]
